9137674e0e11fbb94a51d687d857f0fbe56005ce
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-01-13 21:18:41 +0900

    :recycle: Refactor add root redirect to command form

--- src/main/java/com/command_generator/controller/CommandGeneratorController.java
+++ src/main/java/com/command_generator/controller/CommandGeneratorController.java
@@ -11,29 +11,27 @@ import org.springframework.ui.Model;
 
 // CommandGeneratorController.java
 @Controller
-@RequestMapping("/command")
 public class CommandGeneratorController {
 
     private final CommandGenerator commandGenerator;
 
-    @GetMapping("/")
-    public String index() {
-        return "redirect:/command/form";
-    }
-    @Autowired
     public CommandGeneratorController(CommandGenerator commandGenerator) {
         this.commandGenerator = commandGenerator;
     }
 
-    @GetMapping("/form")
-    public String showForm() {
-        return "command_form";  // /src/main/resources/templates/command_form.html
+    @GetMapping("/") // 이제 도메인/ 접속 시 바로 작동합니다.
+    public String index() {
+        return "redirect:/command/form";
     }
 
-    @RequestMapping(value = "/generate", method = RequestMethod.POST)
+    @GetMapping("/command/form") // 전체 경로를 다 적어줍니다.
+    public String showForm() {
+        return "command_form";
+    }
+
+    @PostMapping("/command/generate")
     public String generateCommand(@ModelAttribute EnchantmentRequest enchantmentRequest, Model model) {
         String command = commandGenerator.generate(enchantmentRequest);
-        System.out.println("Generated command: " + command);  // 생성된 명령어 출력
         model.addAttribute("command", command);
         return "command_form";
     }
